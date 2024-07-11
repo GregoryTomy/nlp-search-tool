af3ed5f796f810a6f2e07530cecf04ef9384a3f6
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2024-07-11 11:32:11 -0600

    doc strings and testing example queries inverted index.

--- src/inverted_index_4.py
+++ src/inverted_index_4.py
@@ -1,14 +1,17 @@
 # %%
+import pprint
+import logging
 from collections import defaultdict
 from typing import Dict, List, Tuple
 from preprocessing_1 import load_spacy_model, tokenize_text
 from query_search_tfidf_3 import load_file
 
-CORPUS_VOCAB_PATH = "../data/corpus_vocab.json"
-CORPUS_PATH = "../data/corpus_tfidf.json"
+CORPUS_VOCAB_PATH = "data/corpus_vocab.json"
+CORPUS_PATH = "data/corpus_tfidf.json"
+EXAMPLE_QUERIES = "data/example_queries.json"
 
-corpus_vocabulary = load_file(CORPUS_VOCAB_PATH)
-corpus = load_file(CORPUS_PATH)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def create_inverted_index(
@@ -23,6 +26,7 @@ def create_inverted_index(
     Returns:
         Dict[Tuple[str, float]]: A dictionary with tokens as keys and a list of tuples (document title, TF-IDF score)  for each token.
     """
+    logger.info("Creating inverted index")
     inverted_index = {}
 
     for doc in corpus:
@@ -36,21 +40,23 @@ def create_inverted_index(
                     inverted_index[token] = []
                 inverted_index[token].append((title, tf_idf_score))
 
+    logger.info("Inverted index successfully created.")
     return inverted_index
 
 
 def search_inverted_index(
     query_tokenized: List[str], inverted_index: Dict[str, Tuple[str, float]]
 ) -> List[Tuple[str, float]]:
-    """_summary_
+    """Search for tokenized terms in the inverted index and return document titles with TF-IDF scores.
 
     Args:
-        query_tokenized (List[str]): _description_
-        inverted_index (Dict[str, Tuple[str, float]]): _description_
+        query_tokenized (List[str]): List of tokenized query terms.
+        inverted_index (Dict[str, Tuple[str, float]]): Inverted index.
 
     Returns:
-        List[Tuple[str, float]]: _description_
+        List[Tuple[str, float]]: List of tuples (document titles, cumulative TF-IDF score) sorted by TF-IDF score in descending order.
     """
+    logger.info("Searching query in inverted index.")
     search = []
     for token in query_tokenized:
         search.extend(inverted_index[token])
@@ -63,4 +69,28 @@ def search_inverted_index(
     # return sorted results
     results = [(title, tf_idf_score) for title, tf_idf_score in results.items()]
 
+    logger.info(f"Search completed. Found {len(results)} matches.")
+
     return sorted(results, key=lambda x: x[1], reverse=True)
+
+
+def main():
+    nlp = load_spacy_model()
+    corpus_vocabulary = load_file(CORPUS_VOCAB_PATH)
+    corpus = load_file(CORPUS_PATH)
+
+    inverted_index = create_inverted_index(corpus_vocabulary, corpus)
+
+    example_queries = load_file(EXAMPLE_QUERIES)
+
+    for query in example_queries:
+        tokenized_query = tokenize_text(query, nlp)
+        results = search_inverted_index(tokenized_query, inverted_index)
+
+        pprint.pprint(f"Query: {query}")
+        pprint.pprint(results)
+        print("\n")
+
+
+if __name__ == "__main__":
+    main()
